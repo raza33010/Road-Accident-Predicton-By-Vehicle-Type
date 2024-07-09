d2e9aefd5b3013333894183e7f6f66c584709fc8
Abbas Raza <raza33010@gmail.com>
2024-07-09 12:14:33 +0500

    commit

--- ML.py
+++ ML.py
@@ -2,14 +2,15 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import numpy as np
 import pickle
+import joblib
 
 app = Flask(__name__)
 CORS(app)
 
 # Load your trained machine learning model
-model = pickle.load(open('DT.pkl', 'rb'))
 
-@app.route('/predict', methods=['POST'])
+
+@app.route('/DT_py/predict', methods=['POST'])
 def predict():
     try:
         # Get data from JSON request
@@ -20,17 +21,84 @@ def predict():
 
         # Convert features to numpy array and reshape as needed
         features = np.array(features).reshape(1, -1)
-
+        model = pickle.load(open('DT.pkl', 'rb'))
         # Make prediction
         prediction = model.predict(features)
-
+        if prediction == 1:
+            response = {'prediction': "cars"}
+        elif prediction ==4 :
+            response = {'prediction': "Taxi"}
+        else:
         # Prepare response in JSON format
-        response = {'prediction': int(prediction[0])}  # Assuming prediction is integer type
+            response = {'prediction': "Others"}  # Assuming prediction is integer type
 
         return jsonify(response)
     
     except Exception as e:
         return jsonify({'error': str(e)}), 400  # Return error message with 400 status code on exception
+    
+    
+
+
+@app.route('/LR_py/predict', methods=['POST'])
+def predictRL():
+    try:
+        # Get data from JSON request
+        data = request.get_json()
+
+        # Extract features dynamically from JSON
+        features = [float(data.get(key)) for key in data.keys()]  # Assuming all values are float
+
+        # Convert features to numpy array and reshape as needed
+        features = np.array(features).reshape(1, -1)
+        # Load your trained machine learning model
+        model = joblib.load('regressor.pkl')
+        # Make prediction
+        prediction = model.predict(features)
+        if prediction == 1:
+            response = {'prediction': "cars"}
+        elif prediction ==4 :
+            response = {'prediction': "Taxi"}
+        else:
+        # Prepare response in JSON format
+            response = {'prediction': "Others"}  # Assuming prediction is integer type
+
+        return jsonify(response)
+    
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400  # Return error message with 400 status code on exception
+    
+
+
+@app.route('/DT/predict', methods=['POST'])
+def predictwithoutpy():
+    try:
+        # Get data from JSON request
+        data = request.get_json()
+
+        # Extract features dynamically from JSON
+        features = [float(data.get(key)) for key in data.keys()]  # Assuming all values are float
+
+        # Convert features to numpy array and reshape as needed
+        features = np.array(features).reshape(1, -1)
+        model = joblib.load('DT_py.pkl')
+        # Make prediction
+        prediction = model.predict(features)
+        if prediction == 1:
+            response = {'prediction': "cars"}
+        elif prediction ==4 :
+            response = {'prediction': "Taxi"}
+        else:
+        # Prepare response in JSON format
+            response = {'prediction': "Others"}  # Assuming prediction is integer type
+
+        return jsonify(response)
+    
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400  # Return error message with 400 status code on exception    
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
